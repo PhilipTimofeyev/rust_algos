ace3caa0f84b2737cb2e6146fe4ca439f292fc6f
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2025-06-11 19:18:29 -0700

    Formatting

--- src/quicksort.rs
+++ src/quicksort.rs
@@ -1,16 +1,26 @@
 #[allow(dead_code)]
 fn quicksort(arr: Vec<i32>) -> Vec<i32> {
-    if arr.len() <= 1 { arr}
-    else {
+    if arr.len() <= 1 {
+        arr
+    } else {
         let pivot = arr[0];
         let mut less = Vec::new();
         let mut greater = Vec::new();
 
         for &i in &arr[1..] {
-            if i <= pivot {less.push(i)} else {greater.push(i)}
+            if i <= pivot {
+                less.push(i)
+            } else {
+                greater.push(i)
+            }
         }
 
-        [quicksort(less).as_slice(), [pivot].as_slice(), quicksort(greater).as_slice()].concat()
+        [
+            quicksort(less).as_slice(),
+            [pivot].as_slice(),
+            quicksort(greater).as_slice(),
+        ]
+        .concat()
     }
 }
 
